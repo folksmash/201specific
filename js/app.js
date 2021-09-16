5425f69fc5ed4af69084ec580b3f468ab5431f93
Jordan Folk <jordan.folk7@gmail.com>
2021-09-15 21:14:23 -0700

    201 loop guessing game added to page and updates per lab 3

--- js/app.js
+++ js/app.js
@@ -2,34 +2,69 @@
 
 let user = prompt('Yo whats your name?')
     alert('Your name is: ' + user)
-let answer1 = confirm('Did you eat your wheaties today?')
-if (answer1 === true) {
-    alert('Obviously, you\'re a champion')
-} else { 
-    alert('Gotta step it up partner')
+
+    let score = 0
+let answer1 = prompt('Is Jordan from Maine?')
+if (answer1.toLowerCase() == 'yes') {
+    alert('Try again')
+} else if (answer1.toLowerCase() == 'no'){ 
+    alert('You\'re correct, I am from Indiana')
+    score++
 }
-let answer2 = confirm('Do you enjoy puns?')
-if (answer2 === true) {
-    alert('PUN OF US PUN OF US')
-} else { 
-    alert('Aww pun on and join us')
+let answer2 = prompt('Did Jordan sail around the world?')
+if (answer2.toLowerCase() == 'yes') {
+    alert('Only in my dreams')
+} else if (answer2.toLowerCase() == 'no'){ 
+    alert('You\'re correct, I\'m not much for water')
+    score++
 }
-let answer3 = confirm('Are you over 6\' tall?')
-if (answer3 === true) {
-    alert('How is the weather up there?')
-} else { 
-    alert('Welcome to the club')
+let answer3 = prompt('Did Jordan play Lacrosse in school?')
+if (answer3.toLowerCase() == 'yes') {
+    alert('Partner, I don\'t even know the rules of the game')
+} else if (answer3.toLowerCase() == 'no'){ 
+    alert('Obviously, You could tell Basketball was more my style')
+    score++
 }
-let answer4 = confirm('Do you club baby seals?')
-if (answer4 === false) {
-    alert('Oh, thank god you don\'t')
-} else { 
-    alert('You must be low on money AND gold')
+let answer4 = prompt('Can Jordan build a PC?')
+if (answer4.toLowerCase() == 'yes') {
+    alert('A fellow gamer I see')
+    score++
+} else if (answer4.toLowerCase() == 'no'){ 
+    alert('Your lack of faith disturbs me')
 }
-let answer5 = confirm('Are you going to excerise tonight?')
-if (answer5 === true) {
-    alert('TIME TO GET PUMPED UP')
-} else { 
-    alert('*sad Arnold noises*')
+let answer5 = prompt('Is Jordan\'s dream job at Google?')
+if (answer5.toLowerCase() == 'yes') {
+    alert('Even after looking it up, you still got the answer wrong?!')
+} else if (answer5.toLowerCase() == 'no'){ 
+    alert('I am more of an Apple guy')
+    score++
 }
-    alert('Thanks for playing along ' + user)
+for (let i = 4; i >= 1; i--){
+    let answer6 = +prompt('How many bones has Jordan broken?');
+    console.log(answer6);
+    let guess = 0;
+    if (answer6 > guess){
+        alert('A little too high')
+    answer6;
+    }
+    else if (answer6 === guess){
+        alert('You got it right')
+        score++
+    break 
+    }
+}
+for (let i = 6; i >= 1; i--){
+    let answer7 = prompt('What is my favorite NBA teams?').toLocaleLowerCase()
+    console.log(answer7)
+    let favteam = ['pacers', 'raptors', 'lakers']
+    if (answer7 === favteam[0]||answer7===favteam[1]||answer7===favteam[2]) {
+        alert('Correct, do you like them too?')
+    score++
+    break
+    }
+    else if (answer7 !== favteam[0]&&answer7!==favteam[1]&&answer7!==favteam[2]){
+        alert('Keep guessing you got this')
+    }
+
+}
+    alert('Thanks for playing along ' + user + ' Your score was ' + score)
